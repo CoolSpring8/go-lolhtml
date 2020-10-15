e74a063e1868554334b758dd76993aaceabe02c1
CoolSpring8 <coolspring888@gmail.com>
2020-10-15 18:54:16 +0800

    fix: fix undefined reference to pow

--- lolhtml.go
+++ lolhtml.go
@@ -3,6 +3,7 @@ package lolhtml
 /*
 #cgo CFLAGS:-I${SRCDIR}/build/include
 #cgo LDFLAGS:-llolhtml
+#cgo !windows LDFLAGS:-lm -ldl
 #cgo linux,amd64 LDFLAGS:-L${SRCDIR}/build/linux-x86_64
 #cgo darwin,amd64 LDFLAGS:-L${SRCDIR}/build/macos-x86_64
 #cgo windows,amd64 LDFLAGS:-L${SRCDIR}/build/windows-x86_64
