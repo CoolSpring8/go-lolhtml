9542959338ba0d320060698babebfb06b8221e61
CoolSpring8 <coolspring888@gmail.com>
2020-10-15 17:15:32 +0800

    fix: switch to prebuilt static lib method

--- lolhtml.go
+++ lolhtml.go
@@ -1,8 +1,11 @@
 package lolhtml
 
 /*
-#cgo CFLAGS: -I/usr/local/include/lolhtml
-#cgo LDFLAGS: /usr/local/lib/lolhtml/liblolhtml.so
+#cgo CFLAGS:-I${SRCDIR}/build/include
+#cgo LDFLAGS:-llolhtml
+#cgo linux,amd64 LDFLAGS:-L${SRCDIR}/build/linux-x86_64
+#cgo darwin,amd64 LDFLAGS:-L${SRCDIR}/build/macos-x86_64
+#cgo windows,amd64 LDFLAGS:-L${SRCDIR}/build/windows-x86_64
 #include <stdlib.h>
 #include "lol_html.h"
 extern void callback_sink(const char *chunk, size_t chunk_len, void *user_data);
